567057d370f6880c8919c8166dcd9f71fd3908b4
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-03-07 15:13:48 -0800

    Update script.js

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ export async function script(octokit, repository, options) {
     repo,
     path,
     sha,
-    //@ts-expect-error
+    // @ts-expect-error
     content: Buffer.from(JSON.stringify(pkg, null, 2) + "\n").toString(
       "base64"
     ),
